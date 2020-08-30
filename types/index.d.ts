1953669541efabd146ac709bdb43ca8a24205376
Mahamed-Belkheir <mohamedbel@protonmail.com>
2020-08-30 17:59:29 +0100

    finished implementing typescript type definitions

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,35 +1,43 @@
-export declare class Qufl {
-    constructor(options: QuflOptions)
+export = Qufl
+
+declare class Qufl {
+    constructor(options: Qufl.QuflOptions);
+
     signToken(options: {sub: string, aud?: string, client?: string, custom: any }): Promise<{ token: string, refresh: string }>
+    
     refreshToken(validToken: { sub: string, client: string }): Promise<string>
+    
     removeToken(validToken: { sub: string, client: string }): Promise<void>
-    getValidator(options: {aud?: string, type?: string, predicate: (token: QuflToken) => boolean }): (req, res, next) => Promise<void>
+    
+    getValidator(options?: {aud?: string, type?: string, predicate?: (token: Qufl.QuflToken) => boolean }): (req, res, next) => Promise<void>
+    
     changeSecret(newString: string): Promise<void>
 }
 
-export declare interface QuflOptions {
-    jwt: JWTInterface
-    secret: string
-    timeout?: number | string
-    useMiddleware?: boolean
+declare namespace Qufl {
+    export interface QuflToken {
+        sub: string
+        aud: string
+        type: string
+        custom?: any
+    }
+    interface JWTInterface {
+        verify(token: string, secret: string): string | object
+        sign(payload: string | object | Buffer, secret: string): string;
+    }
+    interface QuflOptions {
+        jwt: JWTInterface
+        secret: string
+        timeout?: number | string
+        useMiddleware?: boolean
+    }
+
 }
 
-export declare interface JWTInterface {
-    verify(token: string, secret: string): string | object
-    sign(payload: string | object | Buffer, secret: string): string;
-}
-
-export declare interface QuflToken {
-    sub: string
-    aud: string
-    type: string
-    custom: any
-}
-
-declare global{
+declare global {
     namespace Express {
         interface Request {
-            qufl: QuflToken | undefined
+            qufl: Qufl.QuflToken
         }
     }
 }
